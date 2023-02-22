91f7651e2f63e5d0d2dc27d7ec5b2c7fff3fd23b
fanfan5210 <safa.khalouli@gmail.com>
2023-02-22 16:29:48 +0100

    firstcommit

---
+++ kata4/src/main/java/com/ajc/kata4/Kata4Application.java
@@ -0,0 +1,13 @@
+package com.ajc.kata4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata4Application.class, args);
+	}
+
+}
